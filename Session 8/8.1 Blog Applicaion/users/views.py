b378df6aa22432ffc200ff705d259745ddc0d89b
Satyam <satyamsajha@gmail.com>
2025-09-29 12:17:40 +0530

    Minor Project for session 8.1

--- Session 8/8.1 Blog Applicaion/users/views.py
+++ Session 8/8.1 Blog Applicaion/users/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
-    form = UserCreationForm()
-    return render(requet 'users/register.html', {'form': form})
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'User account created! You can login.')
+            return redirect('login')
+    else:
+        form =  UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+    
+    context= {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    
+    return render(request, 'users/profile.html')
